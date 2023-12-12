76db07a301675c7da7ca8b36490a1a0227d58f78
usuario2-CDJ2324 <carolinaedijulio@gmail.com>
2023-12-12 19:43:32 +0100

    agregar cambios

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tNum1 = new System.Windows.Forms.TextBox();
+            this.tNum2 = new System.Windows.Forms.TextBox();
+            this.tNum3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(397, 122);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tNum1
+            // 
+            this.tNum1.Location = new System.Drawing.Point(395, 81);
+            this.tNum1.Name = "tNum1";
+            this.tNum1.Size = new System.Drawing.Size(180, 22);
+            this.tNum1.TabIndex = 1;
+            // 
+            // tNum2
+            // 
+            this.tNum2.Location = new System.Drawing.Point(401, 126);
+            this.tNum2.Name = "tNum2";
+            this.tNum2.Size = new System.Drawing.Size(147, 22);
+            this.tNum2.TabIndex = 2;
+            // 
+            // tNum3
+            // 
+            this.tNum3.Location = new System.Drawing.Point(401, 171);
+            this.tNum3.Name = "tNum3";
+            this.tNum3.Size = new System.Drawing.Size(174, 22);
+            this.tNum3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tNum3);
+            this.Controls.Add(this.tNum2);
+            this.Controls.Add(this.tNum1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tNum1;
+        private System.Windows.Forms.TextBox tNum2;
+        private System.Windows.Forms.TextBox tNum3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1, num2, num3, suma, producto;
+            string tSuma, tProducto;
+
+            num1 = double.Parse(tNum1.Text);
+            num2 = double.Parse(tNum2.Text);
+            num3 = double.Parse(tNum3.Text);
+
+            suma = (double)num1 + (double)num2 + (double)num3;
+            producto = (double)num1 * (double)num2 * (double)num3;
+
+            tSuma = suma.ToString();
+            tProducto = producto.ToString();
+
+            MessageBox.Show("La suma de los números ingresados es: " + suma + " el producto es: " + producto);
+        }
+    }
+}
